4ddd277d32f6000e0a3742b6cbb2bb0b1aecdca3
santhosh <santhosh.gdpu@gmail.com>
2017-02-10 01:03:25 +0530

    comment added

--- app/src/main/java/com/chetan/androidavrodemo/MainActivity.java
+++ app/src/main/java/com/chetan/androidavrodemo/MainActivity.java
@@ -23,6 +23,7 @@ import org.apache.avro.specific.SpecificDatumWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
+//java -jar /home/Hadoop/Avro_work/jars/avro-tools-1.7.7.jar compile schema /home/Hadoop/Avro_work/schema/emp.avsc /home/Hadoop/Avro/with_code_gen
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
